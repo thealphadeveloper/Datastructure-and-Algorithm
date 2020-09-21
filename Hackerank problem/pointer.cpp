c8633156df4939181b88cb7722f968af22ea9c95
Lakshay Varshney <lakshay@alphacoder.me>
2020-09-22 03:40:21 +0530

    Solved pointer problem

---
+++ Hackerank problem/pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void update(int *a,int *b) {
+    int temp1 = (*a + *b);
+    int temp2 = abs(*a - *b);
+    *a = temp1;
+    *b = temp2;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb); 
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
