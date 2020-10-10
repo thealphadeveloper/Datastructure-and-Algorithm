56564e82c0454127a46808a706d5c95d19129228
Lakshay Varshney <lakshay@alphacoder.me>
2020-10-11 03:29:25 +0530

    Climb stairs

---
+++ Dynamic Programming/ClimbStairs.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] dp = new int[n + 1];
+
+        int cp = countPaths(n, dp);
+        System.out.println(cp);
+    }
+
+    public static int countPaths(int n, int[] dp) {
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1];
+            } else if (i == 2) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        }
+
+        return dp[n];
+    }
+
+}
